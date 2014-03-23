2567a9450b44d99eeb28eef2df1db4b10c851d07
Rasesh <RasPat1@gmail.com>
2014-03-23 06:45:08 +0000

    more caching

--- 2009_1A/Happiness.php
+++ 2009_1A/Happiness.php
@@ -5,13 +5,14 @@
 	$path = $vagrantPath;
 
 	$handle = fopen(
-		$path . 'A-small-practice.in',
+		$path . 'A-large-practice.in',
 		'r');
 
 	$out = fopen(
-		$path . 'A-small-practice.out',
+		$path . 'A-large-practice.out',
 		'w');
-
+	$scratchPad = fopen(
+		$path . 'scrachPad.txt', 'w');
 	$test = array();
 
 	if ($handle) {
@@ -33,13 +34,12 @@
 		$memoPad[$n] = array();
 	}
 
-	// echo isMagic(10, 82) ? 'true' : 'false';
-	// $nextB2 = nextMN(9, 6, $memoPad);
-	// print_r($nextB2);
-	// exit;
 	$start = microtime(true);
 	for ($t = 0; $t < $testCases; $t++) {
-		echo $t;
+		$caseStart = microtime(true);
+		$tNum = $t + 1;
+
+		echo "case: $tNum";
 		if (empty($test[$t])) {
 			continue;
 		}
@@ -49,31 +49,45 @@
 		$found = false;
 		$numOfBases = count($bases);
 		$largestBase = $bases[count($bases) - 1];
-
+		$smallestBase = $bases[0];
 		// 1) Take the largest base and find the smallest magic number.
 		// 2) See if it is also magic in the other bases
 		// 3) If yes then done
 		// 4) If not then find the next smallest magic number in the largest base
-		
-
-		$nextSmallestMN = nextMN($largestBase, 2, $memoPad);
+		$nextSmallestMN = nextMN($smallestBase, 2, $memoPad);
 		while ($found === false) {
 
 			$validForAll = true;
-			for ($j = 0; $j < $numOfBases - 1; $j++) {
+			for ($j = 1; $j < $numOfBases; $j++) {
 				$validForAll = $validForAll && isMagic($bases[$j], $nextSmallestMN, $memoPad);
 				if ($validForAll === false) {
 					break;
 				}
-				$memoPad[$bases[$j]][] = $nextSmallestMN;
+				$memoPad[$bases[$j]][$nextSmallestMN] = true;
 			}
 			if ($validForAll === true) {
 				$found = true;
 			} else {
-				$nextSmallestMN = nextMN($largestBase, $nextSmallestMN, $memoPad);
+				$nextSmallestMN = nextMN($smallestBase, $nextSmallestMN, $memoPad);
 			}
 		}
-		$tNum = $t + 1;
+		$caseEnd = microtime(true) - $caseStart;
+		print_r("");
+		echo ' caseEnd: ' . $caseEnd . "\n";
+		// if ($tNum === 3) {
+		// 	foreach ($memoPad as $base => $table) {
+		// 		fwrite($scratchPad, $base);
+		// 		foreach ($table as $key => $value) {
+		// 			fwrite($scratchPad, "\t$key => $value\n");
+		// 		}
+		// 		fwrite($scratchPad, "\n");
+		// 	}
+		// 	// fwrite($scratchPad, implode(" ", implode("\n ", $memoPad)));
+		// 	// print_r(expression);
+		// 	print_r($memoPad);
+		// 	echo isset($memoPad[2][215]) ? 'set' : 'notset';
+		// 	exit;
+		// }
 		fwrite($out, "Case #{$tNum}: {$nextSmallestMN}\n");
 	}
 	$end = microtime(true) - $start;
@@ -81,36 +95,44 @@
 	function nextMN($base, $num, &$memoPad) {
 		$num = $num + 1;
 		while(!isMagic($base, $num, $memoPad)) {
+			$memoPad[$base][$num] = false;
 			$num++;
 		}
-		array_push($memoPad[$base], $num);
-		// print_r($memoPad);
-		// exit;
+		$memoPad[$base][$num] = true;
 		return $num;
 	}
-	function isMagic($base, $num, $memoPad) {
-		if (in_array($num, $memoPad[$base])) {
-			echo 'found ';
-			return true;
+	function isMagic($base, $num, &$memoPad) {
+		if (isset($memoPad[$base][$num])) {
+			return $memoPad[$base][$num];
 		}
 		$visitedNums = array();
 		// Record the number
 		// Reduce the number to its squared thing
 		// Continue until the number has already been reached or it reduces to 1
 		
-		// $visitedNums[] = $num;
-		// $nextNum = reduce($base, $num);
 		while( !in_array($num, $visitedNums) ) {
 			$visitedNums[] = $num;
 			$num = reduce($base, $num);
 			if ($num == 1) {
+				foreach ($visitedNums as $key) {
+					$memoPad[$base][$key] = true;
+				}
 				return true;
+			} else if (isset($memoPad[$base][$num])) {
+				return $memoPad[$base][$num];
 			}
+		}
+		foreach ($visitedNums as $key) {
+			$memoPad[$base][$key] = false;
 		}
 		return false;
 
 	}
-
+	function addToAllToPad($base, $visitedNums, &$memoPad, $magicValue) {
+		foreach ($visitedNums as $key) {
+			$memoPad[$base][$key] = $magicValue;
+		}
+	}
 	//Returns the calc i.e. 81(B10) = 65(B10)
 	function reduce($base, $num) {
 
@@ -118,12 +140,12 @@
 		// 2) Square the digits
 		// 3) Add the digits
 		$num = base_convert($num, 10, $base);
-		$digits = array();
-		while ($num > 0) {
-			$digits[] = $num % 10;
-			$num = $num / 10;
-			$num = intval($num);
-		}
+		$digits = str_split($num);
+		// while ($num > 0.01) {
+		// 	$digits[] = $num % 10;
+		// 	$num = $num / 10;
+		// 	// $num = intval($num);
+		// }
 		$digCount = count($digits);
 		for ($i = 0; $i < $digCount; $i++) {
 			$n = $digits[$i];
